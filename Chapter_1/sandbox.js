6049dead411ac6c114755c492c2406f681e4ea89
jamextech1 <okorojameschizaram@gmail.com>
2022-12-06 17:52:19 +0100

    ci/cd

--- Chapter_1/sandbox.js
+++ Chapter_1/sandbox.js
@@ -1,33 +1,48 @@
-// let getName = prompt("What is your name? ");
-// let getAge = prompt("What is your age? ");
-// console.log("You're , ", getName, "and you're ", getAge, "years old.");
+// // let getName = prompt("What is your name? ");
+// // let getAge = prompt("What is your age? ");
+// // console.log("You're , ", getName, "and you're ", getAge, "years old.");
 
-//  ------
-// strings
-console.log("hello, world");
+// //  ------
+// // strings
+// console.log("hello, world");
+// let email = "mario@thenetninja.co.uk";
+// console.log(email);
+// // sting concatenation
+// // concatenation is joining two strings together. e.g:
+
+// let firstName = "Brandon";
+// let lastName = "Sanderson";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// // getting characters
+// // this is extracting single characters using the squared bracket notation, e.g;
+
+// // did this console from my own though lol.
+// // console.log(fullName[0].toLowerCase().indexOf("b"));
+
+// console.log(fullName[2]);
+// // string length
+// // this helps us to get full length of somthing (string)
+// console.log(fullName.length);
+
+// // string methods
+// // strings has several different functions associated with them, and these functions are called methods.
+// // function                                    || method
+// // this is used to perform some specific task. || This is a function associated with a particular data type||
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
+
+// // another method (index)
+// let index = email.indexOf("@");
+// console.log(index);
+
+// COMMON STRING METHODS
 let email = "mario@thenetninja.co.uk";
-console.log(email);
-// sting concatenation
-// concatenation is joining two strings together. e.g:
-
-let firstName = "Brandon";
-let lastName = "Sanderson";
-let fullName = firstName + " " + lastName;
-console.log(fullName);
-
-// getting characters
-// this is extracting single characters using the squared bracket notation, e.g;
-
-// did this console from my own though lol.
-// console.log(fullName[0].toLowerCase().indexOf("b"));
-
-console.log(fullName[2]);
-// string length
-// this helps us to get full length of somthing (string)
-console.log(fullName.length);
-
-// string methods
-// strings has several different functions associated with them, and these functions are called methods.
-
-// function                                    || method
-// this is used to perform some specific task. || This is a function associated with a particular data type||
+// let result = email.lastIndexOf("n");
+// let result = email.slice(2, 5);
+// let result = email.substr(4, 10);
+// let result = email.replace("m", "w");
+let result = email.replace("n", "w");
+console.log(result);
