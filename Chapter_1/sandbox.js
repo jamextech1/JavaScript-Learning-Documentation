6b38a2f86aece0d1cb9a6f657f63d8cd26cd3506
jamextech1 <okorojameschizaram@gmail.com>
2022-12-06 16:15:54 +0100

    ci/cd

--- Chapter_1/sandbox.js
+++ Chapter_1/sandbox.js
@@ -1,3 +1,24 @@
-let getName = prompt("What is your name? ");
-let getAge = prompt("What is your age? ");
-console.log("You're , ", getName, "and you're ", getAge, "years old.");
+// let getName = prompt("What is your name? ");
+// let getAge = prompt("What is your age? ");
+// console.log("You're , ", getName, "and you're ", getAge, "years old.");
+
+//  ------
+// strings
+console.log("hello, world");
+let email = "mario@thenetninja.co.uk";
+console.log(email);
+// sting concatenation
+// concatenation is joining two strings together. e.g:
+
+let firstName = "Brandon";
+let lastName = "Sanderson";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// getting characters
+// this is extracting single characters using the squared bracket notation, e.g;
+console.log(fullName[0].toLowerCase().indexOf("b"));
+
+// string length
+
+// string methods
