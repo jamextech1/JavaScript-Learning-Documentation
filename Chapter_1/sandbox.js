3b6d1451a555bc12082504ab447f66887e0fa7fd
jamextech1 <okorojameschizaram@gmail.com>
2022-12-06 16:50:13 +0100

    ci/cd

--- Chapter_1/sandbox.js
+++ Chapter_1/sandbox.js
@@ -17,8 +17,17 @@ console.log(fullName);
 
 // getting characters
 // this is extracting single characters using the squared bracket notation, e.g;
-console.log(fullName[0].toLowerCase().indexOf("b"));
 
+// did this console from my own though lol.
+// console.log(fullName[0].toLowerCase().indexOf("b"));
+
+console.log(fullName[2]);
 // string length
+// this helps us to get full length of somthing (string)
+console.log(fullName.length);
 
 // string methods
+// strings has several different functions associated with them, and these functions are called methods.
+
+// function                                    || method
+// this is used to perform some specific task. || This is a function associated with a particular data type||
