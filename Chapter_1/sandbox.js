23b5c3f49ca4dd3cc404f294d7796044a8e52faa
jamextech1 <okorojameschizaram@gmail.com>
2022-12-07 22:34:33 +0100

    ci/cd

--- Chapter_1/sandbox.js
+++ Chapter_1/sandbox.js
@@ -46,3 +46,10 @@ let email = "mario@thenetninja.co.uk";
 // let result = email.replace("m", "w");
 let result = email.replace("n", "w");
 console.log(result);
+
+// play audio
+let song = new Audio("1da_Banton_No_Wahala_9jaflaver.com_-1.mp3");
+let h = document.querySelector("h1");
+h.addEventListener("mouseover", function () {
+  song.play();
+});
