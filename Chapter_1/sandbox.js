4552f47127948780ac2d4c71d4676bef269493b1
jamextech1 <okorojameschizaram@gmail.com>
2022-12-08 16:47:14 +0100

    ci/cd

--- Chapter_1/sandbox.js
+++ Chapter_1/sandbox.js
@@ -46,12 +46,3 @@
 // // another method (index)
 // let index = email.indexOf("@");
 // console.log(index);
-
-// COMMON STRING METHODS
-let email = "mario@thenetninja.co.uk";
-// let result = email.lastIndexOf("n");
-// let result = email.slice(2, 5);
-// let result = email.substr(4, 10);
-// let result = email.replace("m", "w");
-let result = email.replace("n", "w");
-console.log(result);
