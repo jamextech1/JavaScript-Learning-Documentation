3c89c9523959ef682df2bfa057ed562eb11df27d
jamextech1 <okorojameschizaram@gmail.com>
2022-12-11 00:21:47 +0100

    ci/cd

--- Chapter_3/sandbox.js
+++ Chapter_3/sandbox.js
@@ -1,1 +1,14 @@
 // CONTROL FLOW E.G: loops, conditionals i.e if statements, else and else if statements.
+//
+// FOR LOOPS
+//
+// for (let i = 0; i < 5; i++) {
+//   console.log("In loop:", i);
+// }
+// console.log("Loop finished");
+//
+//
+const names = ["shaun", "mario", "luigi"];
+for (let i = 0; i < names.length; i++) {
+  console.log(i);
+}
