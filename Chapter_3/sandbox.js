e97e94d08242495b0f4a13b17d25226ea77c4518
jamextech1 <okorojameschizaram@gmail.com>
2022-12-11 00:34:41 +0100

    ci/cd

--- Chapter_3/sandbox.js
+++ Chapter_3/sandbox.js
@@ -8,7 +8,18 @@
 // console.log("Loop finished");
 //
 //
+// const names = ["shaun", "mario", "luigi"];
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
+//
+//
+//
+//WHILE LOOP
 const names = ["shaun", "mario", "luigi"];
-for (let i = 0; i < names.length; i++) {
-  console.log(i);
+let i = 0;
+while (i < 5) {
+  let html = `<div>${names[i]}</div>`;
+  console.log(html);
+  i++;
 }
