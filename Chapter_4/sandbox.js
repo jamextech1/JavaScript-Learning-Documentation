dd28c0b5aedf167fe80e8c2e15b7e1cc9204eccb
jamextech1 <okorojameschizaram@gmail.com>
2022-12-14 22:03:02 +0100

    ci/cd

--- Chapter_4/sandbox.js
+++ Chapter_4/sandbox.js
@@ -1,12 +1,23 @@
-// FUNCTION DECLARATION
-function greet() {
-  console.log(`Hello there`);
-}
+// FUNCTION DECLARATION : this is setting up a function normally and calling it out.
+// function greet() {
+//   console.log(`Hello there`);
+// }
 
 // FUNCTION EXPRESSION
 // now this is a function expression,  also we don't need to give the functoion a name because the function name is already the variable. Also, at the end of every expression there's always a semi colon, so that's why this function's curly brackets has a semi colon at the end.
-const speak = function () {
-  console.log(`good day!`);
-};
+
+// so basically function expression is just storing a function as a variable.
+// const speak = function () {
+//   console.log(`good day!`);
+// };
 // calling function
-greet();
+// greet();
+// greet();
+// greet();
+//
+//
+// speak();
+// speak();
+// speak();
+
+// Using function declaration, you can call your functions before or after the function command code, WHILE: using a function expression, you can only call your function after the function command code.
