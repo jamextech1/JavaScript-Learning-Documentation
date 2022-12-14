0beb698c3157a604726c6d0af666d560fb1d36a6
jamextech1 <okorojameschizaram@gmail.com>
2022-12-14 21:48:50 +0100

    ci/cd

---
+++ Chapter_4/sandbox.js
@@ -0,0 +1,12 @@
+// FUNCTION DECLARATION
+function greet() {
+  console.log(`Hello there`);
+}
+
+// FUNCTION EXPRESSION
+// now this is a function expression,  also we don't need to give the functoion a name because the function name is already the variable. Also, at the end of every expression there's always a semi colon, so that's why this function's curly brackets has a semi colon at the end.
+const speak = function () {
+  console.log(`good day!`);
+};
+// calling function
+greet();
