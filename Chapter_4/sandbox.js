07d27822e6a2708e5a56121870f9918cdad359dc
jamextech1 <okorojameschizaram@gmail.com>
2022-12-19 15:03:20 +0100

    co/cd

--- Chapter_4/sandbox.js
+++ Chapter_4/sandbox.js
@@ -42,8 +42,10 @@
 // myApp();
 //
 // RETURNING VALUES
-const calcArea = function (radius) {
-  return 3.14 * radius ** 2;
-};
-const area = calcArea(5);
-console.log(area);
+// const calcArea = function (radius) {
+//   return 3.14 * radius ** 2;
+// };
+// const area = calcArea(5);
+// console.log(area);
+//
+// ARROW FUNCTIONS
