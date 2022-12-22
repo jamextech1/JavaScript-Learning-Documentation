37102d8dbdd9e51ef7f7f9d65b6d1a3c1df6fe6b
okorojames <okorojameschizaram@gmail.com>
2022-12-22 22:39:47 +0100

    ci/cd

--- Chapter_6/sandbox.js
+++ Chapter_6/sandbox.js
@@ -1,1 +1,17 @@
 // DOM (Document Object Model)
+// const para = document.querySelector("div.error");
+// console.log(para);
+// selecting all the p elements
+const paras = document.querySelectorAll("p");
+// just like an array we can aslo use forEach method
+// e.g
+paras.forEach((para) => {
+  console.log(para);
+});
+//
+const errors = document.querySelectorAll(".error");
+errors.forEach((error) => {
+  error.style = "color: red";
+  error.style.fontFamily = "Josefin sans";
+});
+console.log(paras[0]);
