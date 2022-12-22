2d07b3ffe85664b4179c433c6ab663702e9b65ee
okorojames <okorojameschizaram@gmail.com>
2022-12-22 22:45:26 +0100

    ci/cd

--- Chapter_6/sandbox.js
+++ Chapter_6/sandbox.js
@@ -2,7 +2,7 @@
 // const para = document.querySelector("div.error");
 // console.log(para);
 // selecting all the p elements
-const paras = document.querySelectorAll("p");
+/*const paras = document.querySelectorAll("p");
 // just like an array we can aslo use forEach method
 // e.g
 paras.forEach((para) => {
@@ -14,4 +14,21 @@ errors.forEach((error) => {
   error.style = "color: red";
   error.style.fontFamily = "Josefin sans";
 });
-console.log(paras[0]);
+console.log(paras[0]);*/
+//
+//
+/*// get an element by id
+const title = document.getElementById("page-title");
+console.log(title);
+//
+//
+
+// get an element by className
+const errors = document.getElementsByClassName("error");
+console.log(errors);
+// you can't use forEach method on HTML collection
+//
+//
+// // get an element by tagName
+const paras = document.getElementsByTagName("p");
+console.log(paras);*/
