94fc08ddc5208fc35a8f07bebb6949c40262aa7e
okorojames <okorojameschizaram@gmail.com>
2022-12-23 12:45:15 +0100

    ci/cd

--- Chapter_6/sandbox.js
+++ Chapter_6/sandbox.js
@@ -97,3 +97,9 @@ const title = document.querySelector("h1");
 // title.setAttribute("style", "margin: 50px");
 //
 // using the javascript style property is the alternative way to style the element without overriding the already existing styles.
+console.log(title.style);
+console.log(title.style.color);
+title.style.margin = "50px";
+title.style.color = "crimson";
+title.style.fontSize = "60px";
+title.style.margin = "";
