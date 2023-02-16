0c8e46aeff6ab86e4bd4db579d57454025fde934
okorojames <okorojameschizaram@gmail.com>
2023-02-16 16:11:06 +0100

    done with building pop up,  moving to form events

--- Chapter_6/popup.js
+++ Chapter_6/popup.js
@@ -1,9 +1,12 @@
 const button = document.querySelector('button')
 const popup = document.querySelector('.popup-wrapper')
-const popupclose = document.querySelector('.popup-close')
+const close = document.querySelector('.popup-close')
 button.addEventListener('click', () => {
   popup.style.top = '0'
 })
-popupclose.addEventListener('click', () => {
+close.addEventListener('click', () => {
+  popup.style.top = '-300%'
+})
+popup.addEventListener('click', () => {
   popup.style.top = '-300%'
 })
