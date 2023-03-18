8f4772bb1c529ca676c80bf6c80d046474b592b8
okorojames <okorojameschizaram@gmail.com>
2023-03-19 00:25:35 +0100

    grasping more light on form validation

--- form-validation/sandbox.js
+++ form-validation/sandbox.js
@@ -1,12 +1,14 @@
 const form = document.querySelector(".signup-form");
 const feedback = document.querySelector(".feedback");
+const usernameInput = document.querySelector("#username");
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
 // const username = document.querySelector("#username")
 form.addEventListener("submit", formSubmit);
 function formSubmit(e) {
   e.preventDefault();
   //
   const username = form.username.value;
-  const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
   //
   if (usernamePattern.test(username)) {
     // good feedback
@@ -20,5 +22,15 @@ function formSubmit(e) {
 //
 // live feedback
 form.username.addEventListener("keyup", (e) => {
-  console.log(e.target.value, form.username.value);
+  e.preventDefault();
+  // console.log(e.target.value, form.username.value);
+  if (usernamePattern.test(e.target.value)) {
+    feedback.textContent = "Successful";
+    usernameInput.classList.add("form--success");
+    usernameInput.classList.remove("form--failed");
+  } else {
+    feedback.textContent = "Not Successs";
+    usernameInput.classList.add("form--failed");
+    usernameInput.classList.remove("form--success");
+  }
 });
