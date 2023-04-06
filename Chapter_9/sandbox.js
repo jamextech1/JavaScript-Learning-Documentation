30c584cbc0c042015996693982175d4ad23d8f39
okorojames <okorojameschizaram@gmail.com>
2023-04-06 17:52:58 +0100

    almost done with the section and moving to the next project

--- Chapter_9/sandbox.js
+++ Chapter_9/sandbox.js
@@ -15,7 +15,7 @@ console.log(premium_users);*/
 //
 //
 // Map method
-const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+/*const prices = [20, 10, 30, 25, 15, 40, 80, 5];
 const saleprices = prices.map((price) => price / 2);
 console.log(saleprices);
 const product = [
@@ -28,4 +28,44 @@ const product = [
 const salesproducts = product.map((product) =>
   product.price > 20 ? product.price / 2 : product.price
 );
-console.log(salesproducts);
+console.log(salesproducts);*/
+//
+//
+// Reduce method
+// const scores = [10, 20, 60, 40, 70, 90, 30];
+// const results = scores.reduce((acc, curr) => {
+//   if (curr > 50) {
+//     acc++;
+//   }
+//   return acc;
+// }, 0);
+// console.log(results);
+//
+/*const scores = [
+  { player: "mario", score: 50 },
+  { player: "yoshi", score: 30 },
+  { player: "mario", score: 70 },
+  { player: "crystal", score: 60 },
+];
+const newScore = scores.reduce((acc, curr) => {
+  if (curr.player === "mario") {
+    acc += curr.score;
+  }
+  return acc;
+}, 0);
+console.log(newScore);*/
+
+//
+//
+// Find method
+/*const scores = [10, 5, 0, 40, 90, 10, 20, 70];
+const ascore = scores.find((score) => {
+  if (score > 45) {
+    return find;
+  }
+});
+console.log(ascore);*/
+//
+//
+// Sort method
+const scores = [10, 5, 0, 40, 90, 10, 20, 70];
