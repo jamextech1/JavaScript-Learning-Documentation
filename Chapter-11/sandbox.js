d90be8d3e037bf6a920467804b1ee1bd1b0cf555
okorojames <okorojameschizaram@gmail.com>
2023-04-22 14:18:30 +0100

    on date object section

---
+++ Chapter-11/sandbox.js
@@ -0,0 +1,7 @@
+const now = new Date();
+console.log(now);
+console.log(typeof now);
+console.log("getFullYear", now.getFullYear());
+console.log("getMonth", now.getMonth());
+console.log("getDate", now.getDate());
+console.log("getDay", now.getDay());
