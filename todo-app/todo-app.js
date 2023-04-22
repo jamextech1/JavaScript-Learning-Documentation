f7bab0506e6173c4701c8ecc8d2674f6d2b00b09
okorojames <okorojameschizaram@gmail.com>
2023-04-22 12:59:58 +0100

    building the filetring of todo feature

---
+++ todo-app/todo-app.js
@@ -0,0 +1,49 @@
+const addForm = document.querySelector(".add");
+const todos = document.querySelector(".todos");
+const searchTodo = document.querySelector(".search");
+//
+// submitting todo
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todoText = addForm.add.value.trim();
+  if (todoText.length) {
+    userDetails(todoText);
+  }
+  addForm.reset();
+});
+//
+// todo template
+const userDetails = (todoText) => {
+  const html = `
+   <li
+          class="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span>${todoText}</span>
+          <i class="far fa-trash-alt delete"></i>
+        </li>
+  `;
+  todos.innerHTML += html;
+};
+// deleting todo
+todos.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
+//
+// searching and filtering todos
+searchTodo.addEventListener("keyup", (e) => {
+  const term = searchTodo.search.value.trim().toLowerCase();
+  e.preventDefault();
+  filteredTodo(term);
+});
+//
+// const newFilteered todos
+const filteredTodo = (term) => {
+  Array.from(todos.children)
+    .filter((todo) => !todo.textContent.includes(term))
+    .forEach((todo) => todo.classList.add("filtered"));
+  Array.from(todos.children)
+    .filter((todo) => todo.textContent.includes(term))
+    .forEach((todo) => todo.classList.remove("filtered"));
+};
