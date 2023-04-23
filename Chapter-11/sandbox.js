7b1800a061038fcf0bbc145110cc22847a7dadf2
okorojames <okorojameschizaram@gmail.com>
2023-04-23 19:28:34 +0100

    still learning async function in javascript

--- Chapter-11/sandbox.js
+++ Chapter-11/sandbox.js
@@ -1,7 +1,19 @@
-const now = new Date();
-console.log(now);
-console.log(typeof now);
-console.log("getFullYear", now.getFullYear());
-console.log("getMonth", now.getMonth());
-console.log("getDate", now.getDate());
-console.log("getDay", now.getDay());
+const clock = document.querySelector(".clock");
+
+const tick = () => {
+  const now = new Date();
+
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
+
+  const html = `
+    <span>${h}</span> :
+    <span>${m}</span> :
+    <span>${s}</span>
+  `;
+
+  clock.innerHTML = html;
+};
+
+setInterval(tick, 1000);
